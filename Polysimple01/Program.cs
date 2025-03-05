cb710dae0580c449bc0f69ad45ad7ce3be071dd8
ST10451846 <161336379+ST10451846@users.noreply.github.com>
2025-03-05 15:22:53 +0200

    Add project files.

---
+++ Polysimple01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polysimple01  /* Method overriding happens when a subclass provides a 
+                          specific implementation of a method that is 
+                           already defined in its base class
+                        */
+{ 
+    class Animal
+    {
+      public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal makes a sound");
+        }
+}
+     
+class Dog: Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog Barks");
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Animal myAnimal = new Dog(); // Parent class reference holding a child class object
+        myAnimal.MakeSound(); // Calls Dog's MakeSound() method 
+        }
+    }
+}
+
